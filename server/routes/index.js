65159b4c1b7d8bc99695ce502af05e48d81699b9
Dominic-Lee-Francis <dominic.l.francis@outlook.com>
2024-02-18 13:55:58 +0800

    commented index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
+/* SET UP ROUTER FOR CRUD OPERATIONS 
+*/
 const express = require("express");
+// imports module for handling user related CRUD operations
 const userController = require("../controller/users.js");
-
 const router = express.Router();
+
+
+// CRUD operations
+// Create a user in the browser. Make sure to use the correct URL: http://localhost:3000/user
 router.post("/user", userController.createUser);
+// Read a user in the browser. Make sure to use the correct URL: http://localhost:3000/user/:username
 router.get("/user/:username", userController.getUser);
 
+// Export the router to be used in the app.js file
 module.exports = router;
 
