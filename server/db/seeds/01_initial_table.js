b95c393937f91585aa1126f7a373a8af3b7ebf32
gmlwisemail2022 <gmlwisemail2004@yahoo.com>
2024-02-19 13:34:40 +0800

    Revert "Update 01_initial_table.js"
    This reverts commit 0e388ae33c55e33aa406a86f1c11d4abd60284f2.

--- server/db/seeds/01_initial_table.js
+++ server/db/seeds/01_initial_table.js
@@ -50,7 +50,7 @@ exports.seed = async function (knex) {
     },
   ]);
 
-  await knex("favorites").insert([
+  await knex("stock").insert([
     { user_id: 1, recipe_id: 1 },
     { user_id: 1, recipe_id: 2 },
     { user_id: 2, recipe_id: 2 },
