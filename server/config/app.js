06a35e815ba2d00b915a93835d1aab23febb8ff2
Dominic-Lee-Francis <dominic.l.francis@outlook.com>
2024-02-20 20:53:01 +0800

    added simple login page (temporary)

--- server/config/app.js
+++ server/config/app.js
@@ -31,5 +31,9 @@ app.get('/', (req, res) => {
     res.render('home.hbs')
 });
 
+app.get('/login', (req, res) => {
+    res.render('login.hbs')
+});
+
 // module is exported to the server
 module.exports = app;
