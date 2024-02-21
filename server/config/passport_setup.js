697112f8ed398fb3d2364e8b52960e31c269476d
Dominic-Lee-Francis <dominic.l.francis@outlook.com>
2024-02-21 22:18:26 +0800

    altered passport.js

--- server/config/passport_setup.js
+++ server/config/passport_setup.js
@@ -1,15 +1,19 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+// passport.use() is a function that takes a new instance of a strategy as its first argument
 passport.use(
-    new GoogleStrategy({
-    // options for the google strategy
+  new GoogleStrategy({
+    // options for the GoogleStrategy
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: '/auth/google/redirect',
-    clientID: 'your google client id',
-    clientSecret: 'your google client secret'
-}, (accessToken, refreshToken, profile, done) => {
-    // passport callback function
-    console.log('passport callback function fired');
-    console.log(profile);
-    // done(null, profile);
-}))
+    passReqToCallback: true
+    }, 
+    (request, accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        // check if user already exists in our db
+        console.log('passport callback function fired');
+        console.log(profile);
+        done(null, profile);
+    }));
