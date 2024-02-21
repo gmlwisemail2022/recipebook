19aab713813b8dfbc778e2a10d7672e713cbf563
Dominic-Lee-Francis <dominic.l.francis@outlook.com>
2024-02-21 23:02:13 +0800

    passport and google testing

--- server/config/passport_setup.js
+++ server/config/passport_setup.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const dotenv = require('dotenv');
+dotenv.config({ path: "../.env"});
 
 // passport.use() is a function that takes a new instance of a strategy as its first argument
 passport.use(
@@ -7,13 +9,24 @@ passport.use(
     // options for the GoogleStrategy
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/redirect',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
     passReqToCallback: true
     }, 
     (request, accessToken, refreshToken, profile, done) => {
         // passport callback function
         // check if user already exists in our db
-        console.log('passport callback function fired');
-        console.log(profile);
+        
         done(null, profile);
     }));
+
+// passport.serializeUser() 
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+// passport.deserializeUser() 
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+module.exports = passport;
--- server/routes/auth_routes.js
+++ server/routes/auth_routes.js
@@ -1,4 +1,21 @@
+const passport = require('passport');
+
 const router = require('express').Router();
+const session = require('express-session');
+router.use(session({ secret: 'anything' }));
+router.use(passport.initialize());
+router.use(passport.session());
+
+
+require('../config/passport_setup.js');
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/auth/login');
+}
+
 
 // auth login
 router.get('/login', (req, res) => {
@@ -13,8 +30,25 @@ router.get('/logout', (req, res) => {
 
 // auth with google
 router.get('/google', (req, res) => {
-    // handle with passport
-    res.send('logging in with google');
+    passport.authenticate('google', {
+        scope: ['email']
+    });
+});
+
+// callback route for google to redirect to
+router.get('/google/callback', (req, res) => {
+    passport.authenticate('google', {
+        successRedirect: '/profile',
+        failureRedirect: '/failure'
+    });
+});
+
+router.get('/failure', (req, res) => {
+    res.send('Failed to log in');
+});
+
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.send('You are logged in');
 });
 
 module.exports = router;
