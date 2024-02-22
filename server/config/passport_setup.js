5abacc30609c7b0bde7e0f089239f360f8a0aded
Dominic-Lee-Francis <dominic.l.francis@outlook.com>
2024-02-22 20:38:30 +0800

    added env link for google client

--- server/config/passport_setup.js
+++ server/config/passport_setup.js
@@ -1,14 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({path: '../.env'});
 
 // passport.use() is a function that takes a new instance of a strategy as its first argument
 passport.use(
   "google", new GoogleStrategy({
     // options for the GoogleStrategy
-    clientID: '586647241181-r9rb78539mgdi0kqtt3qqrml4tbjqj16.apps.googleusercontent.com',
-    clientSecret: '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',
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: 'https://localhost:3000/auth/google/callback',
     // passReqToCallback: true
     }, 
